42e87a80367fef207793129ca63a813f0f7e2209
Shahid Ghafoor <shahidg@live.co.uk>
2022-08-18 14:44:03 +0100

    Added linked lists

---
+++ AlgoAndDataStructures/Linked lists/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoAndDataStructures.Linked_lists
+{
+    public  class Node
+    {
+        public int Data;
+        public Node Next;// to keep the link going
+
+        public Node( int x)
+        {
+            Data = x;
+        }
+
+       
+    }
+
+}
--- AlgoAndDataStructures/Program.cs
+++ AlgoAndDataStructures/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using AlgoAndDataStructures.ADT;
 using AlgoAndDataStructures.ArrInsertion;
+using AlgoAndDataStructures.Linked_lists;
 
 //ADT
 //Car car1 = new Car();              
@@ -36,4 +37,65 @@ using AlgoAndDataStructures.ArrInsertion;
 
 
 //************************************Arrays insert end*********************************************************
-Employee.Insert();
+//Employee.Insert();
+
+//****************************************LINEAR SEARCH***********************************************************
+
+/*int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+bool locate(int[]n,int search)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if(nums[i] == search)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+Console.WriteLine(locate(nums, 5));*/
+
+//**********************LINKED LISTS ********************************
+
+//note that at the end of the list we always have null
+
+Node n1 = new Node(3); // think of these as boxes
+Node n2 = new Node(10);
+Node n3 = new Node(2);
+Node n4 = new Node(26);
+
+n1.Next = n2; // think of this as a link between the boxes
+n2.Next = n3;
+n3.Next = n4;
+
+int countNodes(Node head)
+{
+    int count = 1; // as we already start with a node we increment
+
+    Node current = head; // head now becomes the current node / object
+
+    //as long aas list contains objects until we reach the end of list
+    while(current.Next!=null)
+    {
+        current = current.Next; //traverse through the list next node / object now stored in current varaible
+        // increment the counter
+        count += 1;        
+    }
+    return count;
+}
+
+Console.WriteLine("The amount of nodes: "+ countNodes(n1)); // prints 4 nodes
+
+InsertFirstNode(786, n1);
+
+void InsertFirstNode (int data,Node head)
+{
+    Console.WriteLine("The head was :" + head.Data);
+    Node newNode = new Node(data);
+    newNode.Next = head; //so newNode becomes first
+    Console.WriteLine("The head is now :" + newNode.Data  );
+}
+
+
